a2f217bd1e32bd6f6b5474fcefd38ce5e7bef187
Igor E. Novikov <i.novikov@scalr.com>
2019-07-22 01:57:20 +0300

    track fix

--- build.py
+++ build.py
@@ -84,7 +84,7 @@ if MAIN_BUILD:
     build(LIBPNG, PREFIX)
     build(FREETYPE, PREFIX)
     build(PIXMAN, PREFIX)
-    build(CAIRO, PREFIX, "--disable-xlib", "--disable-ft")
+    build(CAIRO, PREFIX, "--disable-xlib", "--disable-ft", "--disable-lto")
     build(LCMS, PREFIX)
     build(GETTEXT, PREFIX)
     build(PCRE, PREFIX, "--enable-utf", "--enable-unicode-properties")
